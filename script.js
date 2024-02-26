92a1985fdd51fe9422a0fe11763a0939c9630fb8
Maia Werth <maia.moo@me.com>
2024-02-26 01:03:12 -0800

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This is a space where I will show you a few of my favorite things!";
 document.querySelector("body").appendChild(h2);
